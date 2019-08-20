82be1149cde8155132176bfbb301dd1789f04fd9
SerGolub89 <k3210389gsi@gmail.com>
2019-08-20 22:58:25 +0300

    Updates

---
+++ precache-manifest.54df2b29c85aba1676723c4e97609bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675894a31499e8c8c3541e801db99bc5",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "4661c4cf5dc76b1e75c4",
+    "url": "/react_uber-eats/static/css/main.586622b4.chunk.css"
+  },
+  {
+    "revision": "4e0e47c3d37aa84d63a2",
+    "url": "/react_uber-eats/static/js/2.293594df.chunk.js"
+  },
+  {
+    "revision": "4661c4cf5dc76b1e75c4",
+    "url": "/react_uber-eats/static/js/main.85f04970.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.54df2b29c85aba1676723c4e97609bb6.js"
 );
 
 self.addEventListener('message', (event) => {
